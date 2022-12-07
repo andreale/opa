410381fa1cb657a75bb6eb13724cec3ee5e0bc3c
Alessandro Andreani <Alessandro.Andreani@swisscom.com>
2022-12-07 16:37:02 +0100

    New empty rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'cards/erdnuss'
-  get 'cards/well_com'
-  get 'cards/weather'
-  root to: 'visitors#index'
-  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
